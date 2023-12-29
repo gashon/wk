64d88697b77a20ae447f71a10787407566fdfe1e
GashonHussein <gashon96@gmail.com>
2023-12-29 11:22:51 -0700

    feat: useQuery fetch data

--- src/features/workout/api/index.ts
+++ src/features/workout/api/index.ts
@@ -1,1 +1,2 @@
 export * from "./use-create-workout";
+export * from "./use-get-workouts";
---
+++ src/features/workout/api/use-get-workouts.ts
@@ -0,0 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { Days, WorkoutGetRequest, WorkoutGetResponse } from "@/types";
+import { errorNotification } from "@/lib/notification";
+
+const fetchData = async ({
+  type,
+}: WorkoutGetRequest): Promise<WorkoutGetResponse> => {
+  const res = await fetch(`/api/workout?type=${type}`, {
+    method: "GET",
+    mode: "same-origin",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept-Content": "application/json",
+    },
+  });
+  const data = await res.json();
+
+  return data as WorkoutGetResponse;
+};
+
+export const useGetWorkouts = (type: Days) =>
+  useQuery({
+    queryKey: ["workouts"],
+    queryFn: (): ReturnType<typeof fetchData> => fetchData({ type }),
+    refetchOnWindowFocus: false,
+  });
