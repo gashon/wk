c784ee93d2476b3240cef23d3d0dc9dc54a60b1d
GashonHussein <gashon96@gmail.com>
2023-12-30 00:53:47 -0700

    feat: avg data and group by created_at

--- src/features/trends/components/trends-graph.tsx
+++ src/features/trends/components/trends-graph.tsx
@@ -29,6 +29,66 @@ export const TrendsGraph: FC = () => {
 
   const groupedLabel = groupBy("label", data.data);
 
+  const averagedData = Object.fromEntries(
+    Object.entries(groupedLabel).map(([label, data]) => {
+      const groupedByDate = groupBy("created_at_date_string", data);
+      const averaged = Object.entries(groupedByDate).map(([date, entries]) => {
+        const averageWeight =
+          //@ts-ignore
+          entries.reduce((acc, curr) => acc + parseFloat(curr.weight), 0) /
+          entries.length;
+        const averageRepititions =
+          //@ts-ignore
+          entries.reduce((acc, curr) => acc + parseFloat(curr.repititions), 0) /
+          entries.length;
+        return {
+          created_at_date_string: date,
+          weight: averageWeight,
+          repititions: averageRepititions,
+        };
+      });
+      return [label, averaged];
+    }),
+  );
+
+  console.log("av", averagedData);
+
+  return (
+    <>
+      {Object.entries(averagedData).map(([label, data]) => (
+        <div key={`graph:${label}`} className="flex flex-col">
+          <h3>{label}</h3>
+          <ResponsiveContainer width="100%" height={400}>
+            <LineChart
+              width={500}
+              height={300}
+              data={data}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="created_at_date_string" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line
+                type="monotone"
+                dataKey="weight"
+                stroke="#8884d8"
+                activeDot={{ r: 8 }}
+              />
+              <Line type="monotone" dataKey="repititions" stroke="#82ca9d" />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+      ))}
+    </>
+  );
+
   return (
     <>
       {Object.entries(groupedLabel).map(([label, data]) => (
