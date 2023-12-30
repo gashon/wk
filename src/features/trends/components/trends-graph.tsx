cfa05531b4c96948577a95f3496ded5e5dbda0b7
GashonHussein <gashon96@gmail.com>
2023-12-30 00:38:24 -0700

    feat: impl recharts trends

--- src/features/trends/components/trends-graph.tsx
+++ src/features/trends/components/trends-graph.tsx
@@ -9,9 +9,9 @@ import {
   Legend,
   ResponsiveContainer,
 } from "recharts";
-
 import { TrendContext } from "@/features/trends";
 import { DayContext, useGetWorkouts } from "@/features/workout";
+import { groupBy } from "@/util/group-by";
 
 export const TrendsGraph: FC = () => {
   const { startRange, endRange } = useContext(TrendContext);
@@ -27,32 +27,41 @@ export const TrendsGraph: FC = () => {
 
   if (!data) return <p>Error</p>;
 
+  const groupedLabel = groupBy("label", data.data);
+
   return (
-    <ResponsiveContainer width="100%" height="100%">
-      <LineChart
-        width={500}
-        height={300}
-        data={data.data}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="created_at_date_string" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line
-          type="monotone"
-          dataKey="pv"
-          stroke="#8884d8"
-          activeDot={{ r: 8 }}
-        />
-        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-      </LineChart>
-    </ResponsiveContainer>
+    <>
+      {Object.entries(groupedLabel).map(([label, data]) => (
+        <div key={`graph:${label}`} className="flex flex-col">
+          <h3>{label}</h3>
+          <ResponsiveContainer width="100%" height={400}>
+            <LineChart
+              width={500}
+              height={300}
+              data={data}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="created_at_date_string" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line
+                type="monotone"
+                dataKey="weight"
+                stroke="#8884d8"
+                activeDot={{ r: 8 }}
+              />
+              <Line type="monotone" dataKey="repititions" stroke="#82ca9d" />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+      ))}
+    </>
   );
 };
