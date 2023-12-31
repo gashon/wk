4d6826f7f1efb636108f10390c44f7ab21e55c4e
GashonHussein <gashon96@gmail.com>
2023-12-30 20:23:55 -0700

    feat: impl line of best fit

--- src/features/weight/components/weight-graph.tsx
+++ src/features/weight/components/weight-graph.tsx
@@ -1,4 +1,4 @@
-import { FC, useContext } from "react";
+import { FC, useContext, useMemo } from "react";
 import {
   LineChart,
   Line,
@@ -11,18 +11,54 @@ import {
 } from "recharts";
 import { LoadingSkeleton } from "@/components/ui/loading-skeleton";
 import { BodyWeightContext } from "..";
+import { BodyWeight } from "@/types";
+
+const mean = (arr: number[]): number =>
+  arr.reduce((acc, val) => acc + val, 0) / arr.length;
+
+const linearRegression = (
+  bodyWeight: BodyWeight[],
+): { slope: number; intercept: number } => {
+  const x = bodyWeight.map((_, index) => index);
+  const y = bodyWeight.map((item) => item.weight);
+  const xMean = mean(x);
+  const yMean = mean(y);
+  const slope =
+    x.reduce((acc, xVal, idx) => acc + (xVal - xMean) * (y[idx] - yMean), 0) /
+    x.reduce((acc, xVal) => acc + Math.pow(xVal - xMean, 2), 0);
+  const intercept = yMean - slope * xMean;
+  return { slope, intercept };
+};
+
+const generateBestFitLineData = (
+  bodyWeight: BodyWeight[],
+  slope: number,
+  intercept: number,
+): (BodyWeight & { bestFit: number })[] => {
+  return bodyWeight.map((item, index) => ({
+    ...item,
+    bestFit: slope * index + intercept,
+  }));
+};
 
 export const WeightTrendsGraph: FC = () => {
   const { bodyWeightData } = useContext(BodyWeightContext);
 
   if (!bodyWeightData) return <LoadingSkeleton />;
 
+  // Create a deep copy of the bodyWeightData array and reverse it
+  const reversedData = JSON.parse(JSON.stringify(bodyWeightData)).reverse();
+
+  const { slope, intercept } = linearRegression(reversedData);
+  const bestFitLineData = generateBestFitLineData(
+    reversedData,
+    slope,
+    intercept,
+  );
+
   const weights = bodyWeightData.map((item) => item.weight);
   const minY = Math.min(...weights);
   const maxY = Math.max(...weights);
-
-  // Create a deep copy of the bodyWeightData array and reverse it
-  const reversedData = JSON.parse(JSON.stringify(bodyWeightData)).reverse();
 
   return (
     <>
@@ -30,7 +66,7 @@ export const WeightTrendsGraph: FC = () => {
         <LineChart
           width={500}
           height={300}
-          data={reversedData}
+          data={bestFitLineData}
           margin={{
             top: 5,
             right: 30,
@@ -45,9 +81,15 @@ export const WeightTrendsGraph: FC = () => {
           <Legend />
           <Line
             type="monotone"
+            dataKey="bestFit"
+            stroke="#999797"
+            dot={false}
+          />
+          <Line
+            type="monotone"
             dataKey="weight"
             stroke="#000000"
-            activeDot={{ r: 8 }}
+            activeDot={{ r: 2 }}
           />
         </LineChart>
       </ResponsiveContainer>
