864c1c981ec50efc6cd8da3bc831ebf3947b136a
GashonHussein <gashon96@gmail.com>
2024-01-07 23:51:29 -0800

    feat: update @gashon/analytics version

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,6 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
         trackSession
         trackClickEvents
         disableOnDev
+        disableNotifications
       />
       <Toaster />
       <Component {...pageProps} />
